c42142da491d6302a2355622586e342f82883048
lishuang <894597649@qq.com>
2020-04-29 00:09:39 +0800

    shanchu

---
+++ del_install_file.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IF_DELETE_INSTALL_FILE=1
+
+INSTALL_FILE="install"
+
+if [ ${IF_DELETE_INSTALL_FILE} == 1 ];
+then
+    sudo docker exec -it web-app /bin/sh -c 'rm -rf'${INSTALL_FILE}
+    sudo docker exec -it encoder-app /bin/sh -c 'rm -rf'${INSTALL_FILE}
+    echo "Delete finished!"
+fi
