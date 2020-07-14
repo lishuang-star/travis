3a02154dedfc7a8d7ad01ab72ad281cba110ede6
lishuang <894597649@qq.com>
2020-07-14 15:01:09 +0800

    74-CI

--- ci/check_availability.js
+++ ci/check_availability.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const dir = "./ci/screenshots";
-const url = "http://127.0.0.1/";
+const url = "http://127.0.0.1/index.php?m=admin&c=index&a=login";
 
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
@@ -17,8 +17,8 @@ if (!fs.existsSync(dir)) {
   try {
     await page.goto(url);
     await page.screenshot({ path: dir + "/login.png" });
-    await page.type("#username", "huangbo");
-    await page.type("#password", "123456");
+    await page.type("#username", "admin");
+    await page.type("#password", "admin123");
     await page.keyboard.press("Enter", { delay: 3000 });
     await page.screenshot({ path: dir + "/index.png" });
   } catch (e) {
